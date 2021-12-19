0669e98ff1dd3838e5528d2004af52f805357b66
Bálint Fodor <balint.fodor@gmail.com>
2021-12-19 18:44:28 +0100

    Remove logic from main

--- apps/main.cpp
+++ apps/main.cpp
@@ -9,18 +9,5 @@ using namespace ModelMagick;
 
 int main()
 {
-    graph taskGraph;
-
-    auto importer = createModelImporter("fotellab.stl", taskGraph);
-    auto consumer = function_node<Mesh, int>(taskGraph, 1, [](const Mesh& mesh) {
-        cout << mesh.vertices.rows() << " " << mesh.indices.rows() << "\n";
-        return 0;
-    });
-
-    make_edge(importer, consumer);
-
-    importer.activate();
-    taskGraph.wait_for_all();
-
     return 0;
 }
