5579db976e7fe4422e254a4bebe57b40ed38c014
Bálint Fodor <balint.fodor@gmail.com>
2021-12-20 07:32:22 +0100

    Refactor importer and its tests

--- src/model-magick/include/model-magick/MeshImporter.h
+++ src/model-magick/include/model-magick/MeshImporter.h
@@ -10,17 +10,20 @@
 namespace ModelMagick
 {
 
-class MeshImporterError : public std::runtime_error {
+class MeshImporterError : public std::runtime_error
+{
 public:
     MeshImporterError() : std::runtime_error("") {}
 };
 
-class MeshOpenError : public MeshImporterError {};
+class MeshOpenError : public MeshImporterError
+{
+};
 
 Mesh importMesh(const std::filesystem::path& filePath);
 
-oneapi::tbb::flow::input_node<Mesh> createMeshImporter(
-    const std::filesystem::path& filePath,
-    oneapi::tbb::flow::graph& graph);
+oneapi::tbb::flow::function_node<std::filesystem::path, Mesh> createMeshImporter(
+    oneapi::tbb::flow::graph& graph,
+    std::size_t concurrency = oneapi::tbb::flow::unlimited);
 
 }  // namespace ModelMagick
--- tests/test_MeshImporter.cpp
+++ tests/test_MeshImporter.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 
+#include <model-magick/ConstProvider.h>
+#include <model-magick/Consumer.h>
 #include <model-magick/MeshImporter.h>
 
 using namespace std;
@@ -26,17 +28,20 @@ TEST_CASE("Import can load files", "[importer]")
     SECTION("import node with fixed filename") {
 
         graph g;
-        auto importer = createMeshImporter(filename, g);
-        auto consumer = function_node<Mesh, int>(g, 1, [nVertices=nVertices, nFaces=nFaces](const Mesh& mesh) {
-            CHECK(mesh.numVertices() == nVertices);
-            CHECK(mesh.numFaces() == nFaces);
-            return 0;
-        });
+        auto filenameProvider = createConstProvider<filesystem::path>(g, filename);
+        auto importer = createMeshImporter(g);
+        auto consumer = createConsumer<Mesh>(g);
 
+        make_edge(filenameProvider, importer);
         make_edge(importer, consumer);
 
-        importer.activate();
-        REQUIRE_NOTHROW(g.wait_for_all());
+        filenameProvider.activate();
+        g.wait_for_all();
+
+        Mesh resultMesh;
+        CHECK(consumer.try_get(resultMesh));
+        CHECK(resultMesh.numVertices() == nVertices);
+        CHECK(resultMesh.numFaces() == nFaces);
     }
 }
 
@@ -49,14 +54,14 @@ TEST_CASE("Import can throw if the file does not exist", "[importer]")
     SECTION("import node with fixed filename") {
 
         graph g;
-        auto importer = createMeshImporter("NOT-EXISTING-FILE.stl", g);
-        auto consumer = function_node<Mesh, int>(g, 1, [](const Mesh&) {
-            return 0;
-        });
+        auto filenameProvider = createConstProvider<filesystem::path>(g, "NOT-EXISTING-FILE.stl");
+        auto importer = createMeshImporter(g);
+        auto consumer = createConsumer<Mesh>(g);
 
+        make_edge(filenameProvider, importer);
         make_edge(importer, consumer);
 
-        importer.activate();
+        filenameProvider.activate();
         REQUIRE_THROWS_AS(g.wait_for_all(), MeshOpenError);
     }
 }
