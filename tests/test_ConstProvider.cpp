2c93646d0b10f2e6e906b641364230afbce6b259
Bálint Fodor <balint.fodor@gmail.com>
2021-12-20 07:22:38 +0100

    Add test for ConstProvider

--- tests/test_ConstProvider.cpp
+++ tests/test_ConstProvider.cpp
@@ -1,10 +1,9 @@
 #include <catch2/catch.hpp>
 
-#include <tuple>
-#include <vector>
 #include <string>
 
 #include <model-magick/ConstProvider.h>
+#include <model-magick/Consumer.h>
 
 using namespace std;
 using namespace oneapi::tbb::flow;
@@ -13,13 +12,14 @@ using namespace ModelMagick;
 TEST_CASE("ConstProvider can provide a fixed value", "[provider]")
 {
     graph g;
-    auto provider = createConstProvider<std::string>(g, "hello");
-    auto consumer = function_node<std::string, bool>(g, 1, [](const std::string& value) {
-        CHECK(value == "hello");
-        return true;
-    });
+    auto provider = createConstProvider<string>(g, "hello");
+    auto consumer = createConsumer<string>(g);
 
     make_edge(provider, consumer);
     provider.activate();
     g.wait_for_all();
+
+    string out;
+    CHECK(consumer.try_get(out));
+    CHECK(out == "hello");
 }
